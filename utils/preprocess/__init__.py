4a6db13c69439ddef0cad005a1f99c523e205e57
Tushar Pawar <tusharvijaypawar@gmail.com>
2017-12-13 18:29:11 +0530

    Pickled training data paths, labels and vocab

---
+++ utils/preprocess/__init__.py
@@ -0,0 +1,10 @@
+import cv2
+
+
+def image_show(image_object,image_name="Image"):
+    cv2.imshow(image_name, image_object)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+def binarize(image_path):
+    image_object = cv2.imread(image_path)
+    image_show(image_object)
